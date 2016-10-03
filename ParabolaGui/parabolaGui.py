3a2eb673d4d06f2373cea9b3b75633a4220c0cbe
orielbaum <ori.elbaum@gmail.com>
2016-10-04 00:13:49 +0300

    Update parabolaGui.py

--- ParabolaGui/parabolaGui.py
+++ ParabolaGui/parabolaGui.py
@@ -2,6 +2,7 @@
 
 import wx
 import app
+import numpy
 
 class CalcParabula(app.MyFrame):
 	def __init__(self, parent):
@@ -29,17 +30,21 @@ class CalcParabula(app.MyFrame):
 		ylimet = int(self.text_ctrl_6.GetValue())
 		return ylimet
 
+        def pointsDiference(self, event):
+		points_diference = float(self.text_ctrl_7.GetValue())
+		return points_diference	
+
 	def OpenParabula(self, event):
 		import matplotlib.pyplot as plt
 
 		def parabula(a, b, c):
 			ponits = []
-			for i in range(-self.num, self.num+1):
+			for i in numpy.arange(-self.num, self.num+1, self.pointsDiference(1)):
 				point = a*i**2+b*i+c
 				ponits.append(point)
 			return ponits
 
-		plt.plot(range(-self.num, self.num+1), parabula(self.InsertA(1), self.InsertB(1), self.InsertC(1)))
+		plt.plot(numpy.arange(-self.num, self.num+1, self.pointsDiference(1)), parabula(self.InsertA(1), self.InsertB(1), self.InsertC(1)))
 		plt.xlim(-self.xLimet(1), self.xLimet(1))
 		plt.ylim(-self.yLimet(1), self.yLimet(1))
 		plt.grid(True)
